734bb6103049aaafe8a832326cae84bd74c9a7a8
eternal <eternal.git@gmail.com>
2022-11-10 19:14:32 +0000

    Create types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,34 @@
+declare global {
+   interface String {
+      plain: string;
+      reset: string;
+      base: string;
+      bgBase: string;
+      bold: string;
+      normal: string;
+      italic: string;
+      underline: string;
+      inverse: string;
+      hidden: string;
+      strike: string;
+      black: string;
+      red: string;
+      green: string;
+      yellow: string;
+      blue: string;
+      magenta: string;
+      cyan: string;
+      white: string;
+      gray: string;
+      bgBlack: string;
+      bgRed: string;
+      bgGreen: string;
+      bgYellow: string;
+      bgBlue: string;
+      bgMagenta: string;
+      bgCyan: string;
+      bgWhite: string;
+   }
+}
+
+export { };
